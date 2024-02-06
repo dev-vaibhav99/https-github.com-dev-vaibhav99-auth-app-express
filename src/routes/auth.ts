a08ed747aacec00939d8ef9ca4bea02e450677f9
Vaibhav <dev.vaibhav99@gmail.com>
2024-02-06 16:11:44 +0530

    Remove the dead code and comments as well

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,5 @@
 // src/routes/auth.ts
 import { Router, Request, Response } from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { UserRegistration, UserLogin } from "../models/UserInterface";
 import { inputType } from "../middleware/auth";
 import {
   registrationValidation,
@@ -14,13 +11,9 @@ import {
 } from "../controllers/auth-controller";
 
 const router = Router();
-// Example user data (replace this with your database logic)
-const users: UserRegistration[] = [];
 
-// Registration endpoint
 router.post("/register", registrationValidation, registrationController);
 
-// Login endpoint
 router.post("/login", loginValidation, inputType, loginController);
 
 export default router;
