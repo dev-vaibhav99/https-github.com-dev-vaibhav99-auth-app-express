70ccd5e0ffa4cb1020d73914632aaec7add107ca
Vaibhav <dev.vaibhav99@gmail.com>
2024-02-10 19:08:22 +0530

    Move the UserLogin interface at the top and used its properties

--- src/models/UserInterface.ts
+++ src/models/UserInterface.ts
@@ -1,15 +1,13 @@
-export interface UserRegistration {
-  id?: number;
-  firstName: string;
-  lastName: string;
-  email: string;
-  gender: string;
-  mobile: string;
-  password: string;
-  userRole: string;
-}
-
 export interface UserLogin {
   email: string;
   password: string;
+}
+
+export interface UserRegistration extends UserLogin {
+  id?: number;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  mobile: string;
+  userRole: string;
 }
