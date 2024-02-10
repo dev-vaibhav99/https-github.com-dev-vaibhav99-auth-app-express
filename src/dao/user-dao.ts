8740a790a29418afdbf8a9f998e1afc05bc33272
Vaibhav <dev.vaibhav99@gmail.com>
2024-02-10 19:18:47 +0530

    Add getUser method to fetch the data from db

--- src/dao/user-dao.ts
+++ src/dao/user-dao.ts
@@ -6,3 +6,11 @@ export const getAllUsers = async (req: Request, res: Response) => {
   const result = db.collection(userEnums.COLLECTION_NAME).find({}).toArray();
   return result;
 };
+
+export const getUser = async (req: Request, res: Response) => {
+  const db = req.app.locals.db;
+  const result = await db.collection(userEnums.COLLECTION_NAME).findOne({
+    $or: [{ email: req.body.email }, { mobile: req.body.email }],
+  });
+  return result;
+};
